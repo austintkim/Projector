9430af38718c371257d36df811b5a79202cf1d02
Austin Kim <mambafinisher@gmail.com>
2023-11-06 12:48:03 -0800

    Feature 8 complete

--- projects/views.py
+++ projects/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from projects.models import Project
 
 # Create your views here.
+@login_required
 def project_list(request):
-    projects = Project.objects.all()
+    projects = Project.objects.filter(owner = request.user)
     context = {
         "project_list": projects,
     }
