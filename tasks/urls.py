eb9b4c6ae05f4e45e327142472f04187fe5011c8
Austin Kim <mambafinisher@gmail.com>
2023-11-06 15:26:40 -0800

    Feature 15 complete

---
+++ tasks/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from tasks.views import task_create
+
+urlpatterns = [
+    path("create/", task_create, name="create_task")
+]
--- tracker/urls.py
+++ tracker/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path("", redirect_to_project_list, name = "home"),
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("tasks/", include("tasks.urls")),
     path("accounts/", include("accounts.urls")),
 ]
